b04c5fa7a35c0c85f12780a0e6b1283627f96498
agiachris <c.agia@yahoo.com>
2021-06-03 19:18:52 -0400

    edit timeout

--- scenegraph/run.sh
+++ scenegraph/run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 planner=$1
 domain_name=$2
+timeout=$3
 eval "source taskography"
 exp_dir="exp/${domain_name}/"
 exp_name="${planner}"
-eval "python plan.py --domain_name $domain_name --planner $planner --exp-dir $exp_dir --exp-name $exp_name --timeout 60"
+eval "python plan.py --domain_name $domain_name --planner $planner --exp-dir $exp_dir --exp-name $exp_name --timeout $timeout"
