cc711931a13cc26867b3a0d7c6173ebdeff890ad
agiachris <c.agia@yahoo.com>
2021-06-13 21:45:25 -0400

    add runall sh script
    Signed-off-by: agiachris <c.agia@yahoo.com>

--- scenegraph/run.sh
+++ scenegraph/run.sh
@@ -3,6 +3,6 @@ planner=$1
 domain_name=$2
 timeout=$3
 eval "source taskography"
-exp_dir="exp/${domain_name}/"
+exp_dir="exp_official/${domain_name}/"
 exp_name="${planner}"
 eval "python plan.py --domain-name $domain_name --planner $planner --exp-dir $exp_dir --exp-name $exp_name --timeout $timeout"
---
+++ scenegraph/runall.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+declare -a optimal_planners=("FD-seq-opt-lmcut" "SatPlan" "Delfi" "DecStar-opt-fb")
+declare -a satisficing_planners=("FF" "FF-X" "FD-lama-first" "Cerberus-sat" "Cerberus-agl" "DecStar-agl-fb" "bfws")
+declare -a official_grounded_optimal_domains=("v2tiny1" "v2tiny2" "v2medium1" "v2medium2")
+declare -a official_gounded_domains=("v2tiny10" "v2medium10" "v3tiny10bagslots10" "v3medium10bagslots10")
+declare -a official_lifted_domains=("v4tiny5" "v4medium5" "v5tiny5bagslots5" "v5medium5bagslots5")
+declare -a ablation_domains=("v3tiny10bagslots3" "v3tiny10bagslots5" "v3tiny10bagslots7" "v3medium10bagslots3" "v3medium10bagslots5" "v3medium10bagslots7")
+declare -a optimal_timeout=600
+declare -a satisficing_timeout=30
+
+for i in "${official_grounded_optimal_domains[@]}"
+do
+    for j in "${optimal_planners[@]}"
+    do
+        echo "$i $j"
+        cmd="./run.sh $j taskography$i $optimal_timeout"
+        eval $cmd
+    done
+done
+
+for i in "${official_grounded_optimal_domains[@]}"
+do
+    for j in "${satisficing_planners[@]}"
+    do
+        echo "$i $j"
+        cmd="./run.sh $j taskography$i $satisficing_timeout"
+        eval $cmd
+    done
+done
+
+for i in "${official_gounded_domains[@]}"
+do
+    for j in "${satisficing_planners[@]}"
+    do
+        echo "$i $j"
+        cmd="./run.sh $j taskography$i $satisficing_timeout"
+        eval $cmd
+    done
+done
+
+for i in "${official_lifted_domains[@]}"
+do
+    for j in "${satisficing_planners[@]}"
+    do
+        echo "$i $j"
+        cmd="./run.sh $j taskography$i $satisficing_timeout"
+        eval $cmd
+    done
+done
+
+for i in "${ablation_domains[@]}"
+do
+    for j in "${satisficing_planners[@]}"
+    do
+        echo "$i $j"
+        cmd="./run.sh $j taskography$i $satisficing_timeout"
+        eval $cmd
+    done
+done
