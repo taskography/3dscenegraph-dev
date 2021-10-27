68d8c4c511d901a8b162e08c4d863189fb628be1
agiachris <c.agia@yahoo.com>
2021-10-26 18:40:51 -0700

    remove adl2strips clone
    Signed-off-by: agiachris <c.agia@yahoo.com>

--- scenegraph/run.sh
+++ scenegraph/run.sh
@@ -3,6 +3,6 @@ planner=$1
 domain_name=$2
 timeout=$3
 eval "source taskography"
-log_dir="run/"
+log_dir="exp/"
 expid="${planner}"
 eval "python plan.py --domain-name $domain_name --planner $planner --log-dir $log_dir --expid $expid --timeout $timeout"
